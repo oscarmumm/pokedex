baf94835b5d8ad6c83daf442bf7acb3a389abd69
Oscar Mumm <oscarmumm@gmail.com>
2023-03-20 14:53:13 -0300

    light shutdown emergency commit

--- app/app.js
+++ app/app.js
@@ -2,6 +2,13 @@ const d = document;
 const searchInput = d.getElementById('search-input');
 const searchBtn = d.getElementById('search-btn');
 
+const pokemonPicture = d.getElementById('pokemon-picture');
+const pokemonName = d.getElementById('pokemon-name-and-number')
+const pokemonType = d.getElementById('pokemon-type')
+const pokemonHeight = d.getElementById('pokemon-height')
+const pokemonWeight = d.getElementById('pokemon-weight')
+const pokemonAbilities = d.getElementById('pokemon-abilities')
+
 
 const getPokemonInfo = (pokemonName) => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
@@ -12,3 +19,7 @@ const getPokemonInfo = (pokemonName) => {
 searchBtn.addEventListener('click', () => {
   getPokemonInfo(searchInput.value);
 });
+
+const showInfo = (data) => {
+  pokemonName.innerText = `${data.name}`;
+}
