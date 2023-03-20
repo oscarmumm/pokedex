ef022f022ee151692bf444490459a92ec35fa54a
Oscar Mumm <oscarmumm@gmail.com>
2023-03-20 16:13:34 -0300

    functional. initial release

--- app/app.js
+++ app/app.js
@@ -2,8 +2,11 @@ const d = document;
 const searchInput = d.getElementById('search-input');
 const searchBtn = d.getElementById('search-btn');
 
+const pokedexInnerScreen = d.getElementById('pokedex-inner-screen');
+
 const pokemonPicture = d.getElementById('pokemon-picture');
-const pokemonName = d.getElementById('pokemon-name-and-number')
+const pokemonName = d.getElementById('pokemon-name')
+const pokemonNumber = d.getElementById('pokemon-number')
 const pokemonType = d.getElementById('pokemon-type')
 const pokemonHeight = d.getElementById('pokemon-height')
 const pokemonWeight = d.getElementById('pokemon-weight')
@@ -13,7 +16,7 @@ const pokemonAbilities = d.getElementById('pokemon-abilities')
 const getPokemonInfo = (pokemonName) => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
   .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((data) => showInfo(data))
 };
 
 searchBtn.addEventListener('click', () => {
@@ -21,5 +24,13 @@ searchBtn.addEventListener('click', () => {
 });
 
 const showInfo = (data) => {
+  console.log(data)
+  pokedexInnerScreen.style.background = '#b3efff';
+  pokemonPicture.src = `${data.sprites.front_default}`;
   pokemonName.innerText = `${data.name}`;
+  pokemonNumber.innerText = `N° ${data.id}`;
+  pokemonType.innerText = `Type: ${data.types[0].type.name}`;
+  pokemonHeight.innerText = `Height: ${(data.height)*0.1}m`
+  pokemonWeight.innerText = `Weight: ${(data.weight)*0.1}kg`
+  pokemonAbilities.innerText = `Abilities: ${data.abilities[0].ability.name}`
 }
