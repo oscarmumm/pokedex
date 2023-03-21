7821a856ff3ff8a4d099e41b5b6f039971912a87
Oscar Mumm <oscarmumm@gmail.com>
2023-03-21 16:28:51 -0300

    added error msg, btn functionality and footer

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 const d = document;
+
+const lightBlueLight = d.getElementById('lightblue-light');
+const redLight = d.getElementById('red-light');
+const yellowLight = d.getElementById('yellow-light');
+const greenLight = d.getElementById('green-light');
+
 const searchInput = d.getElementById('search-input');
 const searchBtn = d.getElementById('search-btn');
+const previousBtn = d.getElementById('previous-btn');
+const nextBtn = d.getElementById('next-btn');
 
 const pokedexInnerScreen = d.getElementById('pokedex-inner-screen');
 
@@ -14,13 +22,24 @@ const pokemonAbilities = d.getElementById('pokemon-abilities')
 
 
 const getPokemonInfo = (pokemonName) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-  .then((response) => response.json())
-  .then((data) => showInfo(data))
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) => showInfo(data))
+    .catch(error => {
+      showError();
+    })
 };
 
 searchBtn.addEventListener('click', () => {
   getPokemonInfo((searchInput.value).toLowerCase());
+});
+
+previousBtn.addEventListener('click', () => {
+  getPokemonInfo(parseInt(lastSearchId) - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+  getPokemonInfo(parseInt(lastSearchId) + 1);
 });
 
 const stringToCapítalize = (string) => {
@@ -28,7 +47,6 @@ const stringToCapítalize = (string) => {
 }
 
 const showInfo = (data) => {
-  console.log(data)
   pokedexInnerScreen.style.background = 'var(--screen-on-bg)';
   pokemonPicture.src = `${data.sprites.front_default}`;
   pokemonName.innerText = `${stringToCapítalize(data.name)}`;
@@ -37,4 +55,20 @@ const showInfo = (data) => {
   pokemonHeight.innerText = `Height: ${((data.height)*0.1).toFixed(2)}m`;
   pokemonWeight.innerText = `Weight: ${((data.weight)*0.1).toFixed(2)}kg`;
   pokemonAbilities.innerText = `Abilities: ${data.abilities[0].ability.name}`;
+  redLight.classList.remove('light-on');
+  lightBlueLight.classList.add('light-on');
+  return lastSearchId = `${data.id}`;
 };
+
+const showError = () => {
+  pokedexInnerScreen.style.background = 'var(--screen-on-bg)';
+  pokemonPicture.src = '';
+  pokemonNumber.innerText = '';
+  pokemonType.innerText = '';
+  pokemonHeight.innerText = '';
+  pokemonWeight.innerText = '';
+  pokemonAbilities.innerText = '';
+  pokemonName.innerText = `Error en la búsqueda. Revise que el nombre esté bien escrito`;
+  redLight.classList.add('light-on');
+  lightBlueLight.classList.remove('light-on');
+} 
